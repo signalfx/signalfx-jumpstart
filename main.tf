426f2d2eb392be260c5b9b5323047266fbafc54a
Robert Castley <rwc@Roberts-iMac.lan>
2019-10-19 16:42:17 +0100

    Added Lambda detector

--- main.tf
+++ main.tf
@@ -1,21 +1,6 @@
 provider "signalfx" {
-  auth_token     = "${var.access_token}"
-  api_url = "https://api.${var.realm}.signalfx.com"
-}
-# Every resource needs a unique name
-resource "signalfx_detector" "application_delays" {
-    name = "Customer latency is high"
-    description = "SLI metric for customer experienced latency is higher than expectations"
-    program_text = <<-EOF
-signal = data('app.delay_seconds').max()
-detect(when(signal > 3, '1m')).publish('Latency High')
-    EOF
-  rule {
-        detect_label = "Latency High"
-        description = "Latency is high for the last minute"
-        severity = "Critical"
-        #notifications = ["Teamabc123"] # Team ID goes here, send em a message!
-  }
+  auth_token = "${var.access_token}"
+  api_url    = "https://api.${var.realm}.signalfx.com"
 }
 
 resource "signalfx_detector" "httpcode_elb_5xx" {
@@ -29,5 +14,32 @@ detect(when(((error/total)*100) >= 10, lasting='5m')).publish('ELB 10% of reques
   rule {
         detect_label = "ELB 10% of requests were 5xx for last 5m"
         severity = "Critical"
+  }
+}
+
+resource "signalfx_detector" "cpu_historical_norm" {
+    name = "[SFx] CPU utilization % greater than historical norm"
+    description = "Alerts when CPU usage for this host for the last 10 minutes was significantly higher than normal, as compared to the last 24 hours"
+    program_text = <<-EOF
+from signalfx.detectors.against_recent import against_recent
+A = data('cpu.utilization').publish(label='A', enable=False)
+against_recent.detector_mean_std(stream=A, current_window='10m', historical_window='24h', fire_num_stddev=3, clear_num_stddev=2.5, orientation='above', ignore_extremes=True, calculation_mode='vanilla').publish('CPU utilization is significantly greater than normal, and increasing')
+    EOF
+  rule {
+        detect_label = "CPU utilization is significantly greater than normal, and increasing"
+        severity = "Warning"
+  }
+}
+
+resource "signalfx_detector" "lambda_error_rate" {
+    name = "[SFx] Lambda High Error Rate"
+    description = "Lambda function error rate is greater than 10 for the last 5m"
+    program_text = <<-EOF
+errors = data('Errors', filter=(filter('namespace', 'AWS/Lambda') and filter('FunctionName', '*') and filter('Resource', '*') and filter('stat', 'sum'))).publish(label='errors', enable=False)
+detect((when(errors > 10, lasting='5m'))).publish('Lambda function error rate is greater than 10 for the last 5m')
+    EOF
+  rule {
+        detect_label = "Lambda function error rate is greater than 10 for the last 5m"
+        severity = "Major"
   }
 }
