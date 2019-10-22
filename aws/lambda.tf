ea8de12750e2ac22e10a72336ebdcde3b364ef41
Robert Castley <robert.castley@gmail.com>
2019-10-22 20:54:16 +0100

    Formatting changes and filename change

--- aws/lambda.tf
+++ aws/lambda.tf
@@ -11,28 +11,28 @@ detect((when(errors > 10, lasting='5m'))).publish('AWS/Lambda function error rat
   }
 }
 resource "signalfx_detector" "lambda_historical_duration_error" {
-    name = "[SFx] AWS/Lambda Historical duration error"
-    description = "AWS/Lambda Lambda duration has been greater then historical norm during the past 10 minutes"
-    program_text = <<-EOF
+  name         = "[SFx] AWS/Lambda Historical duration error"
+  description  = "AWS/Lambda Lambda duration has been greater then historical norm during the past 10 minutes"
+  program_text = <<-EOF
 from signalfx.detectors.against_periods import against_periods
 A = data('Duration', filter=filter('namespace', 'AWS/Lambda')).mean().publish(label='A', enable=False)
 against_periods.detector_mean_std(stream=A, window_to_compare='15m', space_between_windows='60m', num_windows=4, fire_num_stddev=3, clear_num_stddev=2, discard_historical_outliers=True, orientation='above').publish('AWS/Lambda Lambda duration has been greater then historical norm during the past 10 minutes')
     EOF
   rule {
-        detect_label = "AWS/Lambda Lambda duration has been greater then historical norm during the past 10 minutes"
-        severity = "Minor"
-    }
+    detect_label = "AWS/Lambda Lambda duration has been greater then historical norm during the past 10 minutes"
+    severity     = "Minor"
+  }
 }
 resource "signalfx_detector" "lambda_historical_coldstart_count_error" {
-    name = "[SFx] AWS/Lambda Historical coldstart count error"
-    description = "AWS/Lambda Lambda coldstart count has been greater then historical norm during the past 10 minutes"
-    program_text = <<-EOF
+  name         = "[SFx] AWS/Lambda Historical coldstart count error"
+  description  = "AWS/Lambda Lambda coldstart count has been greater then historical norm during the past 10 minutes"
+  program_text = <<-EOF
 from signalfx.detectors.against_periods import against_periods
 A = data('aws.lambda.coldStarts').publish(label='A', enable=False)
 against_periods.detector_mean_std(stream=A, window_to_compare='10m', space_between_windows='24h', num_windows=4, fire_num_stddev=3, clear_num_stddev=2.5, discard_historical_outliers=True, orientation='above').publish('AWS/Lambda Lambda coldstart count has been greater then historical norm during the past 30 minutes')
     EOF
   rule {
-        detect_label = "AWS/Lambda Lambda coldstart count has been greater then historical norm during the past 30 minutes"
-        severity = "Warning"
+    detect_label = "AWS/Lambda Lambda coldstart count has been greater then historical norm during the past 30 minutes"
+    severity     = "Warning"
   }
 }
--- dashboards/usage-overview-dashboard-group.tf
+++ dashboards/usage-overview-dashboard-group.tf
@@ -2,4 +2,3 @@ resource "signalfx_dashboard_group" "usageoverview0" {
   name        = "Usage Overview (Terraform)"
   description = "Host Based Model, MTS and Events Usage"
 }
-
