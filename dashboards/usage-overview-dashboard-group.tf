4a546cf2c58dc6cbb9887033e1150c3057fb4694
Robert Castley <robert.castley@gmail.com>
2019-10-23 19:05:38 +0100

    Added [SFx] prefix

--- dashboards/usage-overview-dashboard-group.tf
+++ dashboards/usage-overview-dashboard-group.tf
@@ -1,4 +1,4 @@
 resource "signalfx_dashboard_group" "usageoverview0" {
-  name        = "Usage Overview (Terraform)"
+  name        = "[SFx] Usage Overview (Terraform)"
   description = "Host Based Model, MTS and Events Usage"
 }
