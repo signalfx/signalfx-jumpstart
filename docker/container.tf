0780161066a4add012e49eae61903413cbc1d1e7
Robert Castley <rwc@robertc.lan>
2019-10-25 23:30:54 +0100

    Signal not published so not displaying in UI

--- docker/container.tf
+++ docker/container.tf
@@ -2,9 +2,9 @@ resource "signalfx_detector" "container_cpu_utilization" {
   name         = "[SFx] Container CPU utilization % high"
   description  = "Alerts when CPU Utilization % is between 70% & 80% for 10mins and > 80% for 5mins"
   program_text = <<-EOF
-    A = data('cpu.usage.total', filter=filter('plugin', 'docker'), extrapolation='null', maxExtrapolations=-1)
-    B = data('cpu.usage.system', filter=filter('plugin', 'docker'), extrapolation='null', maxExtrapolations=-1)
-    C = (A/B * 100).publish('Container CPU')
+    A = data('cpu.usage.total', filter=filter('plugin', 'docker')).publish(label='A', enable=False)
+    B = data('cpu.usage.system', filter=filter('plugin', 'docker')).publish(label='B', enable=False)
+    C = (A/B*100).publish(label='Container CPU')
     detect(when(C > 80, lasting='5m')).publish('Container CPU utilization % is above 80 for 5m')
     detect(when(not (C > 80) and not (C < 70), lasting='10m')).publish('Container CPU utilization % is within 70 and 80 for 10m')
   EOF
--- host/cpu.tf
+++ host/cpu.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "cpu_historical_norm" {
   description  = "Alerts when CPU usage for this host for the last 10 minutes was significantly higher than normal, as compared to the last 24 hours"
   program_text = <<-EOF
     from signalfx.detectors.against_recent import against_recent
-    A = data('cpu.utilization').publish(label='A', enable=False)
+    A = data('cpu.utilization').publish(label='A', enable=True)
     against_recent.detector_mean_std(stream=A, current_window='10m', historical_window='24h', fire_num_stddev=3, clear_num_stddev=2.5, orientation='above', ignore_extremes=True, calculation_mode='vanilla').publish('CPU utilization is significantly greater than normal, and increasing')
   EOF
   rule {
