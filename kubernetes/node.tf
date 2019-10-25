67917ebd2d8f406fcef0dafd52bde0efc32320e1
Achim Staebler <astaebler@signalfx.com>
2019-10-25 14:45:55 +0200

    add cpu imbalance detector

--- kubernetes/node.tf
+++ kubernetes/node.tf
@@ -1,1 +1,17 @@
+resource "signalfx_detector" "k8s_" {
+  name         = "[SFx] K8S Cluster CPU balance"
+  description  = "Alerts when cluster CPU usage is imbalanced"
+  program_text = <<-EOF
+    A = data('container_cpu_utilization', filter=filter('kubernetes_cluster', '*') and filter('kubernetes_node', '*'), rollup='rate').sum(by=['kubernetes_node', 'kubernetes_cluster']).publish(label='A', enable=False)
+    B = data('container_cpu_utilization', filter=filter('kubernetes_cluster', '*') and filter('kubernetes_node', '*')).sum(by=['kubernetes_node']).mean(by=['kubernetes_cluster']).publish(label='B', enable=False)
+    C = ((A-B)/B).stddev(by=['kubernetes_cluster']).publish(label='C', enable=False)
+    D = data('kube_node_info', filter=filter('kubernetes_cluster', '*'), rollup='count').count(by=['kubernetes_cluster']).publish(label='D', enable=False)
+    E = (C*D).publish(label='E')
+  EOF
+  rule {
+    detect_label = "K8S Cluster CPU usage is imbalanced"
+    severity = "Critical"
+    disabled = true
+  }
+}
 
