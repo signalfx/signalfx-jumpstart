c99940edc8edfae23dee88001d091823dc327be4
Robert Castley <robert.castley@gmail.com>
2019-10-25 13:09:18 +0100

    Added disk space running out detector

--- host/disk.tf
+++ host/disk.tf
@@ -1,10 +1,13 @@
-resource "signalfx_detector" "disk_partition_space_low" {
+resource "signalfx_detector" "disk_space_low" {
   name         = "[SFx] Low Disk Space"
-  description  = "Alerts when space on root partition is at 80% or 90%"
+  description  = "Alerts when a partition is filling up or total disk space will fill up within 24hrs"
   program_text = <<-EOF
-    disk_usage = data('disk.utilization', filter=(not filter('plugin_instance', 'root'))).publish(label='disk_usage', enable=False)
-    detect(when(disk_usage >= 90)).publish('Disk space has filled upto 90%')
-    detect(when(disk_usage >= 80 and disk_usage < 90)).publish('Disk space has filled upto 80%')
+    A = data('disk.utilization', filter=(not filter('plugin_instance', 'root'))).publish(label='disk_usage', enable=False)
+    detect(when(A >= 90)).publish('Disk space has filled upto 90%')
+    detect(when(A >= 80 and A < 90)).publish('Disk space has filled upto 80%')
+    from signalfx.detectors.countdown import countdown
+    B = data('disk.summary_utilization').publish(label='B', enable=False)
+    countdown.hours_left_stream_incr_detector(stream=B, maximum_capacity=100, lower_threshold=24, fire_lasting=lasting('15m', 1), clear_threshold=36, clear_lasting=lasting('15m', 1), use_double_ewma=False).publish('Disk space utilization is projected to reach 100% within 24 hours')
   EOF
   rule {
     detect_label = "Disk space has filled upto 80%"
@@ -14,4 +17,9 @@ resource "signalfx_detector" "disk_partition_space_low" {
     detect_label = "Disk space has filled upto 90%"
     severity     = "Critical"
   }
+  rule {
+    detect_label = "Disk space utilization is projected to reach 100% within 24 hours"
+    severity     = "Critical"
+  }
+
 }
