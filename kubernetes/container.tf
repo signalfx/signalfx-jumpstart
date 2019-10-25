a549779b97938b535f0aed868d3b9e6d2fc14474
Achim Staebler <astaebler@signalfx.com>
2019-10-25 14:46:26 +0200

    update descriptions

--- kubernetes/container.tf
+++ kubernetes/container.tf
@@ -13,16 +13,17 @@ resource "signalfx_detector" "k8s_container_restarts" {
 }
 
 resource "signalfx_detector" "k8s_container_cpu" {
-  name         = "[SFx] Container CPU utilization is higher than normal"
-  description  = "Container CPU utilization (%) in the last 5m is more than 2.5 standard deviations above the mean of its preceding 30m"
+  name         = "[SFx] Container CPU utilization is higher than normal, and increasing"
+  description  = "Alerts when container CPU utilization (%) in the last 5m is more than 2.5 standard deviations above the mean of its preceding 30m"
   program_text = <<-EOF
     from signalfx.detectors.against_recent import against_recent
     A = data('container_cpu_percent', filter=filter('kubernetes_cluster', '*') and filter('kubernetes_namespace', '*') and filter('sf_tags', '*', match_missing=True) and filter('deployment', '*', match_missing=True), rollup='rate').publish(label='A', enable=False)
     against_recent.detector_mean_std(stream=A, current_window='5m', historical_window='30m', fire_num_stddev=2.5, clear_num_stddev=2, orientation='above', ignore_extremes=True, calculation_mode='vanilla').publish('K8S CPU% Detector')
   EOF
   rule {
-    detect_label = "K8S Container CPU Usage higher than normal"
-    severity = "Critical"
+    detect_label = "K8S Container CPU Usage higher than normal, and increasing"
+    severity = "Warning"
+    disabled = true
   }
 }
 
