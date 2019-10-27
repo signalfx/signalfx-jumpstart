15d132dd5a50c3758993aa6a8523fdc177c8c281
Pieter Hagen <phagen@signalfx.com>
2019-10-27 16:54:22 +0100

    GardenHealthCheckFailed added

--- pivotal/DiegoCM.tf
+++ pivotal/DiegoCM.tf
@@ -15,6 +15,7 @@ CPM = data('rep.CapacityRemainingMemory', filter=filter('metric_source', 'cloudf
 CapacityRemainingMemory = (CPM/1024).publish(label='B', enable=True)
 B = data('rep.RepBulkSyncDuration', filter=filter('metric_source', 'cloudfoundry'), rollup='max').mean().publish(label='B', enable=True)
 RepBulkSyncDuration = (B/1000000000).mean(over='5m').publish(label='RepBulkSyncDuration', enable=True)
+GardenHealthCheckFailed = data('rep.GardenHealthCheckFailed', filter=filter('metric_source', 'cloudfoundry'), rollup='max').max(over='5m').publish(label='GardenHealthCheckFailed', enable=True)
 
 not_reporting.detector(stream=CPM, resource_identifier=None, duration='15m').publish('Pivotal Cloudfoundry - CapacityRemainingMemory not being reported.')
 detect(when((CapacityRemainingMemory > 32) and (CapacityRemainingMemory <= 64))).publish('Pivotal Cloudfoundry - CapacityRemainingMemory 5 Minute Minumum is within 32GB  and 64GB.')
@@ -22,6 +23,9 @@ detect(when(CapacityRemainingMemory <= 32)).publish('Pivotal Cloudfoundry - Capa
 countdown.hours_left_stream_detector(stream=CapacityRemainingDisk, minimum_value=6, lower_threshold=48, fire_lasting=lasting('12m', 0.95), clear_threshold=60, clear_lasting=lasting('12m', 0.95), use_double_ewma=False).publish('Pivotal Cloudfoundry - CapacityRemainingDisk - (assumed to be decreasing) is projected to decrease to 6 in 48 hour(s).')
 detect(when((RepBulkSyncDuration >= 5) and (RepBulkSyncDuration < 10))).publish('Pivotal Cloudfoundry - RepBulkSyncDuration - Average Response is within 5 and 10 seconds.')
 detect(when(RepBulkSyncDuration >=10)).publish('Pivotal Cloudfoundry - RepBulkSyncDuration - Average Response is over 10 seconds.')
+detect(when(GardenHealthCheckFailed == 1)).publish('Pivotal Cloudfoundry - The value of rep.GardenHealthCheckFailed is 1.')
+detect(when(GardenHealthCheckFailed > 1)).publish('Pivotal Cloudfoundry - The value of rep.GardenHealthCheckFailed is above 1.')
+
     EOF
   rule {
     detect_label = "Pivotal Cloudfoundry - CapacityRemainingMemory not being reported."
@@ -39,12 +43,22 @@ detect(when(RepBulkSyncDuration >=10)).publish('Pivotal Cloudfoundry - RepBulkSy
     detect_label = "Pivotal Cloudfoundry - CapacityRemainingDisk - (assumed to be decreasing) is projected to decrease to 6 in 48 hour(s)."
     severity     = "Minor"
   }
-rule {
+  rule {
     detect_label = "Pivotal Cloudfoundry - RepBulkSyncDuration - Average Response is within 5 and 10 seconds."
     severity     = "Minor"
   }
   rule {
     detect_label = "Pivotal Cloudfoundry - RepBulkSyncDuration - Average Response is over 10 seconds."
+    severity     = "Critical"
+  }
+
+  rule {
+    detect_label = "Pivotal Cloudfoundry - The value of rep.GardenHealthCheckFailed is 1."
+    severity     = "Minor"
+  }
+
+  rule {
+    detect_label = "Pivotal Cloudfoundry - The value of rep.GardenHealthCheckFailed is above 1."
     severity     = "Critical"
   }
 
