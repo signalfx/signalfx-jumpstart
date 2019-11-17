dafd0c14a349a3fbe6c2f9fafc5b3410dfcc841d
Robert Castley <robert.castley@gmail.com>
2019-11-17 19:55:15 +0000

    Change signals to use [A-Z] so is visualised in UI

--- aws/elb.tf
+++ aws/elb.tf
@@ -1,13 +1,15 @@
 resource "signalfx_detector" "httpcode_elb_5xx" {
-  name         = "[SFx] AWS/ELB has high 5xx response ratio"
-  description  = "Alerts when 10% of requests were 5xx for last 5m"
+  name         = "[SFx] AWS/ELB has high 5XX response ratio"
+  description  = "Alerts when 10% of requests were 5XX for last 5m"
+  
   program_text = <<-EOF
-    error = data('HTTPCode_ELB_5XX', filter=(filter('namespace', 'AWS/ELB') and filter('stat', 'count') and filter('LoadBalancerName', '*'))).publish(label='error', enable=False)
-    total = data('RequestCount', filter=(filter('namespace', 'AWS/ELB') and filter('stat', 'count') and filter('LoadBalancerName', '*'))).publish(label='total', enable=False)
-    detect(when(((error/total)*100) >= 10, lasting='5m')).publish('AWS/ELB 10% of requests were 5xx for last 5m')    
+    A = data('HTTPCode_ELB_5XX', filter=(filter('namespace', 'AWS/ELB') and filter('stat', 'count') and filter('LoadBalancerName', '*'))).publish(label='HTTPCode_ELB_5XX', enable=False)
+    B = data('RequestCount', filter=(filter('namespace', 'AWS/ELB') and filter('stat', 'count') and filter('LoadBalancerName', '*'))).publish(label='RequestCount', enable=False)
+    detect(when(((A/B)*100) >= 10, lasting='5m')).publish('AWS/ELB 10% of requests were 5XX for last 5m')    
   EOF
+
   rule {
-    detect_label       = "AWS/ELB 10% of requests were 5xx for last 5m"
+    detect_label       = "AWS/ELB 10% of requests were 5XX for last 5m"
     severity           = "Critical"
     parameterized_body = "${var.message_body}"
   }
