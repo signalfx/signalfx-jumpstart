48e1cc9c81ea77b14ff08d1fb0388882cf5b2a35
Robert Castley <robert.castley@gmail.com>
2019-11-17 20:05:18 +0000

    Tided up message_body and removed hard-coded references

--- host/variables.tf
+++ host/variables.tf
@@ -1,28 +1,34 @@
 variable "message_body" {
   type    = "string"
+
   default = <<-EOF
-{{#if anomalous}}
-	Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" triggered at {{timestamp}}.
-{{else}}
-	Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" cleared at {{timestamp}}.
-{{/if}}
+    {{#if anomalous}}
+	    Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" triggered at {{timestamp}}.
+    {{else}}
+	    Rule "{{{ruleName}}}" in detector "{{{detectorName}}}" cleared at {{timestamp}}.
+    {{/if}}
 
-{{#if anomalous}}
-Triggering condition: {{{readableRule}}}
-{{/if}}
+    {{#if anomalous}}
+      Triggering condition: {{{readableRule}}}
+    {{/if}}
 
-{{#if anomalous}}Signal value for Integration Host Count: {{inputs.A.value}}
-{{else}}Current signal value for Integration Host Count: {{inputs.A.value}}
-{{/if}}
+    {{#if anomalous}}
+      Signal value: {{inputs.A.value}}
+    {{else}}
+      Current signal value: {{inputs.A.value}}
+    {{/if}}
 
-{{#notEmpty dimensions}}
-Signal details:
-{{{dimensions}}}
-{{/notEmpty}}
+    {{#notEmpty dimensions}}
+      Signal details: {{{dimensions}}}
+    {{/notEmpty}}
 
-{{#if anomalous}}
-{{#if runbookUrl}}Runbook: {{{runbookUrl}}}{{/if}}
-{{#if tip}}Tip: {{{tip}}}{{/if}}
-{{/if}}
-EOF
+    {{#if anomalous}}
+      {{#if runbookUrl}}
+        Runbook: {{{runbookUrl}}}
+      {{/if}}
+      {{#if tip}}
+        Tip: {{{tip}}}
+      {{/if}}
+    {{/if}}
+  EOF
 }
